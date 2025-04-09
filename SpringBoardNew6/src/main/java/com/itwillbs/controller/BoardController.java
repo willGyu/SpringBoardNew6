a1445780bf933240e250274b90e06f97138c0b67
willGyu <jgt8731@itwillbs.co.kr>
2025-04-09 17:48:45 +0900

    게시판 글 정보 입력 생성완료(view페이지-템플릿 적용완료)

--- SpringBoardNew6/src/main/java/com/itwillbs/controller/BoardController.java
+++ SpringBoardNew6/src/main/java/com/itwillbs/controller/BoardController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.itwillbs.service.BoardService;
 
 @Controller
+@RequestMapping(value = "/board/*")
 public class BoardController {
 
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
@@ -20,7 +21,9 @@ public class BoardController {
 	@Inject
 	private BoardService bService;
 	
-	// http://localhost:8088/controller/time
+	// http://localhost:8088/controller/time (x)
+	// http://localhost:8088/controller/board/time (x) 
+	// http://localhost:8088/board/time 
 	@RequestMapping(value = "/time",method = RequestMethod.GET)
 	public void getServerTimeGET(Model model) {
 		logger.info(" DB서버의 시간정보를 가져오기 ");
@@ -33,9 +36,20 @@ public class BoardController {
 		
 	}
 	
+	// http://localhost:8088/board/regist
+	// 게시판 글쓰기 - GET
+	@RequestMapping(value = "/regist",method = RequestMethod.GET)
+	public void registGET() throws Exception {
+		logger.info(" registGET() 실행 ");
+		logger.info(" 글쓰기 뷰페이지(/board/regist.jsp)를 연결해서 보여줌 ");		
+	}
+	
+	// 게시판 글쓰기 - POST
 	
 	
 	
 	
 	
-}
+	
+	
+}// BoardController
