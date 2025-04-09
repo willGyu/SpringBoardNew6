b5d800eabb35656c8ba856aae31bd3c52f65fdcd
willGyu <jgt8731@itwillbs.co.kr>
2025-04-09 16:07:25 +0900

    BoardController 생성 완료

---
+++ SpringBoardNew6/src/main/java/com/itwillbs/controller/BoardController.java
@@ -0,0 +1,17 @@
+package com.itwillbs.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BoardController {
+
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
+	
+	
+	
+	
+	
+}
