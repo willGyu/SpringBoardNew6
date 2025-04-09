c6996d0adab5dbb3061ffda3257fc1fd85ee59a9
willGyu <jgt8731@itwillbs.co.kr>
2025-04-09 16:24:55 +0900

    실행할 메서드 생성 ( 주소, 처리방식 매핑 ),연결된 뷰페이지 (JSP) 생성 완료

--- SpringBoardNew6/src/main/java/com/itwillbs/controller/BoardController.java
+++ SpringBoardNew6/src/main/java/com/itwillbs/controller/BoardController.java
@@ -1,13 +1,37 @@
 package com.itwillbs.controller;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.itwillbs.service.BoardService;
 
 @Controller
 public class BoardController {
 
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
+	//BoardService 객체 주입
+	@Inject
+	private BoardService bService;
+	
+	// http://localhost:8088/controller/time
+	@RequestMapping(value = "/time",method = RequestMethod.GET)
+	public void getServerTimeGET(Model model) {
+		logger.info(" DB서버의 시간정보를 가져오기 ");
+		
+		String time = bService.getServerTime();
+		logger.info(" time : {}",time);
+		
+		model.addAttribute("time", time);
+		logger.info(" 연결된 뷰페이지에 정보 전달 ");
+		
+	}
 	
 	
 	
---
+++ SpringBoardNew6/src/main/webapp/WEB-INF/views/time.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+	
+	<%@ include file="include/header.jsp" %>
+	
+	<div class="info-box bg-green">
+            <span class="info-box-icon"><i class="ion ion-ios-heart-outline"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">
+              		time.jsp
+              </span>
+              <span class="info-box-number">
+              		시간 : ${time }
+              </span>
+
+              <div class="progress">
+                <div class="progress-bar" style="width: 20%"></div>
+              </div>
+              <span class="progress-description">
+                    20% Increase in 30 Days
+                  </span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+
+	
+	
+	<%@ include file="include/footer.jsp" %>
